79d79c025c10b4734a6e45e7919f2e2cbfeb7b06
nociza <azicon@berkeley.edu>
2023-12-27 15:08:19 +0800

    Update file changes and add new files

--- src/data/PageLinks.ts
+++ src/data/PageLinks.ts
@@ -1,87 +1,88 @@
 export const corePages = [
-	{ name: 'About us', href: '/about' },
-	{
-		name: 'Start therapy',
-		href: '/therapy',
-	},
-]
+  { name: "About us", href: "/about" },
+  {
+    name: "Performances",
+    href: "/performances",
+  },
+];
 
 export const supportPages = [
-	{
-		name: 'Careers',
-		href: '/careers',
-		icon: '/icons/Careers.svg',
-	},
-	{
-		name: 'Volunteer',
-		href: '/volunteer',
-		icon: '/icons/Volunteer.svg',
-	},
-	{
-		name: 'Partner',
-		href: '/partner',
-		icon: '/icons/Partner.svg',
-	},
+  {
+    name: "Musicians",
+    href: "/audition",
+    icon: "/icons/Careers.svg",
+  },
+  {
+    name: "Students",
+    href: "/students",
+    icon: "/icons/Volunteer.svg",
+  },
+  {
+    name: "Partnerships",
+    href: "/partner",
+    icon: "/icons/Partner.svg",
+  },
 
-	{
-		name: 'Contact',
-		href: '/contact',
-		icon: '/icons/Contact.svg',
-	},
-]
+  {
+    name: "Contact",
+    href: "/contact",
+    icon: "/icons/Contact.svg",
+  },
+];
 
 export const fundraisePages = [
-	{
-		name: 'Join the guild',
-		href: '/guild',
-		context: 'Become a member of our legendary community.',
-		image: '/icons/Guild.webp',
-	},
-	{
-		name: 'Live stream fundraiser',
-		href: '/stream',
-		context: 'Empower your community to change the world.',
-		image: '/icons/Stream.webp',
-	},
-]
+  {
+    name: "Join the community",
+    href: "/newsletter",
+    context:
+      "Sign up to our news letter to get the latest updates on performances and events.",
+    image: "/icons/Guild.webp",
+  },
+  {
+    name: "Live stream fundraiser",
+    href: "/stream",
+    context: "Empower your community to change the world.",
+    image: "/icons/Stream.webp",
+  },
+];
 
 export const mobileNav = [
-	{
-		name: 'About us',
-		href: '/about',
-	},
-	{
-		name: 'RAD Careers',
-		href: '/careers',
-	},
-	{
-		name: 'Volunteering',
-		href: '/volunteer',
-	},
-	{
-		name: 'Partnerships',
-		href: '/partner',
-	},
+  {
+    name: "About us",
+    href: "/about",
+  },
+  {
+    name: "RAD Careers",
+    href: "/careers",
+  },
+  {
+    name: "Volunteering",
+    href: "/volunteer",
+  },
+  {
+    name: "Partnerships",
+    href: "/partner",
+  },
 
-	{
-		name: 'Join the guild',
-		href: '/guild',
-	},
-	{
-		name: 'Stream fundraiser',
-		href: '/stream',
-	},
-]
+  {
+    name: "Join the guild",
+    href: "/guild",
+  },
+  {
+    name: "Stream fundraiser",
+    href: "/stream",
+  },
+];
 
 export const footerPages = [
-	{ name: 'About', href: '/about' },
-	{ name: 'Therapy', href: '/therapy' },
-	{ name: 'Donate', href: '/donate' },
-	{ name: 'Stream', href: '/stream' },
-	{ name: 'Careers', href: '/careers' },
-	{ name: 'Volunteer', href: '/volunteer' },
-	{ name: 'Partner', href: '/partner' },
-	{ name: 'Contact', href: '/contact' },
-	{ name: 'Privacy', href: '/privacy' },
-	{ name: 'Terms', href: '/terms' },
-]
+  { name: "About", href: "/about" },
+  { name: "Therapy", href: "/therapy" },
+  { name: "Donate", href: "/donate" },
+  { name: "Stream", href: "/stream" },
+  { name: "Careers", href: "/careers" },
+  { name: "Volunteer", href: "/volunteer" },
+  { name: "Partner", href: "/partner" },
+  { name: "Contact", href: "/contact" },
+  { name: "Privacy", href: "/privacy" },
+  { name: "Terms", href: "/terms" },
+];
--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,14 +1,14 @@
-import Link from 'next/link'
+import Link from "next/link";
 
 export default function Custom400() {
   return (
     <section>
-      <h1>404</h1>
+      <h1>Sorry, Page not found</h1>
       <p>
         <Link href="/">
           <a>Go to home page</a>
         </Link>
       </p>
     </section>
-  )
+  );
 }
