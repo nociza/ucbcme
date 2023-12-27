fceeb82500603c3d54a90bc626545ffc92ae8e3c
nociza <azicon@berkeley.edu>
2023-12-28 00:47:43 +0800

    Remove unused Tweet components

--- src/pages/careers.tsx
+++ src/pages/careers.tsx
@@ -1,9 +1,8 @@
-import HeaderSection from '@components/HeaderSection'
-import PageSEO from '@components/SEO/PageSEO'
-import Tweet from '@components/Tweet'
-import { careerData, TeamTestimonials } from '@data/radTeamData'
-import SectionGrid from '@layouts/SectionGrid'
-import Image from 'next/image'
+import HeaderSection from "@components/HeaderSection";
+import PageSEO from "@components/SEO/PageSEO";
+import { careerData, TeamTestimonials } from "@data/radTeamData";
+import SectionGrid from "@layouts/SectionGrid";
+import Image from "next/image";
 
 export default function Careers() {
   return (
@@ -32,23 +31,22 @@ export default function Careers() {
           headerTextHighlightColor="from-secondary-light to-primary-light"
           headerTextHighlightBlock
         >
-          Engage with a purpose driven team or enjoy working solo. Work when you&apos;re inspired or
-          rest when you&apos;re not. We believe the ideal work environment comes from putting the
-          mental health of our team first.
+          Engage with a purpose driven team or enjoy working solo. Work when
+          you&apos;re inspired or rest when you&apos;re not. We believe the
+          ideal work environment comes from putting the mental health of our
+          team first.
         </HeaderSection>
-
-        <SectionGrid>
-          <Tweet TwitterPostData={TeamTestimonials} />
-        </SectionGrid>
       </section>
 
       <section className="bg-black text-white rounded-3xl px-4 sm:px-6 lg:px-8">
         <header>
           <h2 className="text-center text-white">
-            <span className="block text-base text-extrabold text-primary-light">Team Benefits</span>
+            <span className="block text-base text-extrabold text-primary-light">
+              Team Benefits
+            </span>
             Benefits that empower you to
             <span className="text-transparent sm:block bg-gradient-to-r from-secondary-light to-primary-light bg-clip-text">
-              {' '}
+              {" "}
               live your best life
             </span>
           </h2>
@@ -56,12 +54,16 @@ export default function Careers() {
         <div className="grid gap-10 sm:grid-cols-3">
           {careerData.map((career) => (
             <div key={career.id}>
-              <div className={`rounded-xl border-4 p-4 text-left h-full ${career.border}`}>
+              <div
+                className={`rounded-xl border-4 p-4 text-left h-full ${career.border}`}
+              >
                 <div key={career.id}>
                   <Image src={career.icon} width={48} height={48} alt="" />
                 </div>
                 <div className="p-1">
-                  <h4 className={`font-semibold text-base ${career.color}`}>{career.id}</h4>
+                  <h4 className={`font-semibold text-base ${career.color}`}>
+                    {career.id}
+                  </h4>
                   <p className="text-white">{career.desc}</p>
                 </div>
               </div>
@@ -70,5 +72,5 @@ export default function Careers() {
         </div>
       </section>
     </div>
-  )
+  );
 }
