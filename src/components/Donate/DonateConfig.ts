de336c2b10e7a01509387802ecf082b2ef0c2063
nociza <azicon@berkeley.edu>
2024-04-08 18:57:41 -0700

    Remove DonateTwitchSub component and update DonateConfig prices

--- src/components/Donate/DonateConfig.ts
+++ src/components/Donate/DonateConfig.ts
@@ -1,27 +1,27 @@
 export const DonateOnceConfig = [
   {
-    priceId: "price_1JVIqXEavBxf0OLS7wmeK3rS",
-    priceLabel: "$30",
-    value: 3000,
+    priceId: "price_1JVIqXEavBxf0OLS7wmeK3rS", //TODO: Change priceId
+    priceLabel: "$10",
+    value: 1000,
+    message: " covers the cost of one set of Erhu strings.",
+  },
+  {
+    priceId: "price_1JVIqXEavBxf0OLS1NQzttQR", //TODO: Change priceId
+    priceLabel: "$50",
+    value: 5000,
     message: " covers the cost of one Erhu bow.",
   },
   {
-    priceId: "price_1JVIqXEavBxf0OLS1NQzttQR",
-    priceLabel: "$60",
-    value: 6000,
-    message: " covers the cost of two Erhu bows.",
-  },
-  {
     priceId: "price_1JVIqXEavBxf0OLS9LMxBJKd",
-    priceLabel: "$120",
-    value: 12000,
-    message: " covers the cost of one new Dizi.",
+    priceLabel: "$100",
+    value: 10000,
+    message: " covers the cost of half a new Dizi.",
   },
   {
     priceId: "price_1JVIqXEavBxf0OLSUbTR8x93",
-    priceLabel: "$300",
-    value: 30000,
-    message: " covers the cost of two new Dizi.",
+    priceLabel: "$500",
+    value: 50000,
+    message: " covers the cost of two new Dizis.",
   },
 ];
 
@@ -40,14 +40,26 @@ export const DonateMonthlyConfig = [
   },
   {
     priceId: "price_1JVIqXEavBxf0OLS9LMxBJKd",
-    priceLabel: "$60",
-    value: 6000,
-    message: " covers the cost of half a Dizi each month.",
+    priceLabel: "$50",
+    value: 5000,
+    message: " lets us save up for two new Dizis in a year.",
   },
   {
     priceId: "price_1JVIqXEavBxf0OLSUbTR8x93",
-    priceLabel: "$120",
-    value: 12000,
-    message: " covers the cost of one Dizi each month.",
+    priceLabel: "$100",
+    value: 10000,
+    message: " lets us save up for a new Erhu in a year.",
+  },
+  {
+    priceId: "price_1JVIqXEavBxf0OLSUbTR8x93",
+    priceLabel: "$200",
+    value: 20000,
+    message: " lets us save up for a new Guzheng in a year.",
+  },
+  {
+    priceId: "price_1JVIqXEavBxf0OLSUbTR8x93",
+    priceLabel: "$500",
+    value: 50000,
+    message: " lets us save up for a new percussion set in a year.",
   },
 ];
